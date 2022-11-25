31b988ce3c10e8def2413d85381277b614683272
Jeetpatel8037 <pateljeet341@gmail.com>
2022-11-25 10:02:30 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/appstoreandplaystore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.appstoreandplaystore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:appstoreandplaystore/Android_Play_Store/Plat_Store.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Play_Store(),
+  ));
+}
